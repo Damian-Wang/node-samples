1a6c7865f7be99338d992c25c17a6ac1141a9bbe
Damian Wang <Damian.t.wang@gmail.com>
2021-07-06 06:55:12 +1000

    Adding events to notion calendar

--- calendar/quickstart/index.js
+++ calendar/quickstart/index.js
@@ -1,23 +1,18 @@
-/**
- * @license
- * Copyright Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// [START calendar_quickstart]
+/* eslint-disable quote-props */
+/* eslint-disable quotes */
+/* eslint linebreak-style: ["error", "windows"]*/
+
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
+
+// we use a few enviornment variables
+const dotenv = require('dotenv').config();
+// npm install @notionhq/client
+const {Client} = require('@notionhq/client');
+const { addAbortSignal } = require('stream');
+const { testing } = require('googleapis/build/src/apis/testing');
+const notion = new Client({auth: process.env['NOTION_KEY']});
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
@@ -88,11 +83,11 @@ function getAccessToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function listEvents(auth) {
+  // List events in google calendar
   const calendar = google.calendar({version: 'v3', auth});
   calendar.events.list({
     calendarId: 'primary',
     timeMin: (new Date()).toISOString(),
-    maxResults: 10,
     singleEvents: true,
     orderBy: 'startTime',
   }, (err, res) => {
@@ -104,14 +99,136 @@ function listEvents(auth) {
         const start = event.start.dateTime || event.start.date;
         console.log(`${start} - ${event.summary}`);
       });
+      // Write to NotionDB
+      writeToDB(events);
     } else {
       console.log('No upcoming events found.');
     }
   });
 }
-// [END calendar_quickstart]
 
-module.exports = {
-  SCOPES,
-  listEvents,
-};
+
+/**
+ * Write to NotionDB
+ * @param {events} events Events in an object as defined by google calendar API
+ */
+async function writeToDB(events) {
+  // Find the first database this bot has access to
+  // TODO(blackmad): move to notion.search()
+  const databases = await notion.databases.list({});
+
+  if (databases.results.length === 0) {
+    throw new Error("This bot doesn't have access to any databases!");
+  }
+
+  const database = databases.results[0];
+  if (!database) {
+    throw new Error("This bot doesn't have access to any databases!");
+  }
+  
+  // Get the database properties out of our database
+  const {properties} = await notion.databases.retrieve({
+    database_id: database.id,
+  });
+  console.log(properties)
+
+  for (i = 0; i < events.length; i++) {
+    const text = events[i].summary;
+    const start_date = events[i].start.dateTime || events[i].start.date;
+    console.log(start_date);
+    let end_date = null;
+    if (start_date.includes("T")) {
+      end_date = events[i].end.dateTime || events[i].end.date;
+        if (end_date.includes("00:00:00")) {
+          tzoffset = new Date(end_date).getTimezoneOffset() * 60000;
+          change_date = Date.parse(end_date);
+          change_date = change_date - 60000 - tzoffset;
+          end_date = new Date(change_date).toISOString("en-US", {timeZone: "Australia/Sydney"});
+        }
+    }
+
+    const event = {
+      "start_date": start_date,
+      "end_date": end_date,
+      "text": text,
+    };
+    await addItem(event);
+  }
+  // await exerciseWriting(database.id, properties, events);
+}
+
+/**
+ * Write to notion DB
+ * @param {string} databaseId Database String
+ * @param {PropertyMap} properties Database property object
+ * @param {events} events Events in an object as defined by google calendar API
+ */
+async function exerciseWriting(databaseId, properties, events) {
+  console.log('\n\n********* Exercising Writing *********\n\n');
+
+  // const RowsToWrite = 10;
+  for (i = 0; i < events.length; i++) {
+    const event = events[0];
+    const start = event.start.dateTime || event.start.date;
+
+    const propertiesValues = {
+      type: "title",
+      // id: property.id,
+      title: [
+        {
+          type: "text",
+          text: {content: start},
+        },
+      ],
+    };
+    await notion.pages.create({
+      parent: {
+        database_id: databaseId,
+      },
+      properties: propertiesValues,
+    });
+  }
+  console.log(`Wrote ${RowsToWrite} rows after ${startTime}`);
+}
+
+/**
+ * Add item to notionDB
+ * @param {JSON} event JSON Event
+ */
+async function addItem(event) {
+  try {
+    await notion.request({
+      path: "pages",
+      method: "POST",
+      body: {
+        parent: {database_id: process.env.NOTION_DATABASE_ID},
+        properties: {
+          title: {
+            title: [
+              {
+                "text": {
+                  "content": event.text,
+                },
+              },
+            ],
+          },
+          "Date":{
+            "date":{
+              "start": event.start_date,
+              "end": event.end_date,
+            },
+          },
+          "Type": {
+            "select": {
+              "name": "Damian's Calendar",
+            },
+          },
+        },
+      },
+    });
+    console.log("Success! Entry added.");
+  } catch (error) {
+    console.error(error.body);
+  }
+}
+
